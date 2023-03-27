97f0a6f8564279cf3eb5ce51db19d6b1cf716f9e
light3739 <51126304+light3739@users.noreply.github.com>
2023-03-27 17:22:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import telebot
 import calendar
 import mysql.connector
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot import types
 
 BOT_TOKEN = '5873352611:AAG1jcPiabZdUSLUStLBNROhjWr98NkNONo'
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -17,25 +17,36 @@ conn = mysql.connector.connect(
     database="mydatabase"
 )
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS notes
-                (date DATE,
-                 chat_id INT,
-                 text TEXT)''')
+cursor.execute('''create table if not exists notes (date date, chat_id int, text text, smiley text)''')
 
 
 @bot.message_handler(commands=['add'])
 def add(message):
     bot.send_message(chat_id=message.chat.id, text='Введите текст:')
-    bot.register_next_step_handler(message, add_text)
+    bot.register_next_step_handler(message, lambda m: ask_for_smiley(m))
 
 
-def add_text(message):
-    date = datetime.now().date()
+def ask_for_smiley(message):
     text = message.text
     chat_id = message.chat.id
-    cursor.execute("INSERT INTO notes (date, chat_id, text) VALUES (%s, %s, %s)", (date, chat_id, text))
+
+    # Create a custom keyboard with the available emojis
+    keyboard = types.ReplyKeyboardMarkup(row_width=3)
+    emoji1 = types.KeyboardButton(text='😊')
+    emoji2 = types.KeyboardButton(text='😢')
+    emoji3 = types.KeyboardButton(text='🙂')
+    keyboard.add(emoji1, emoji2, emoji3)
+
+    bot.send_message(chat_id, 'Выберите смайлик:', reply_markup=keyboard)
+    bot.register_next_step_handler(message, lambda m: add_note_with_smiley(m, text, chat_id, m.text))
+
+
+def add_note_with_smiley(message, text, chat_id, smiley):
+    date = datetime.now().date()
+    cursor.execute("INSERT INTO notes (date, chat_id, text, smiley) VALUES (%s, %s, %s, %s)",
+                   (date, chat_id, text, smiley))
     conn.commit()
-    bot.send_message(chat_id=chat_id, text="Запись добавлена.")
+    bot.send_message(chat_id, 'Заметка добавлена!',  reply_markup=types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=['show'])
@@ -45,14 +56,15 @@ def show(message):
 
 
 def show_func(message):
-    date = message.text # получаем дату из сообщения
+    date = message.text  # получаем дату из сообщения
     cursor.execute("SELECT text FROM notes WHERE date = %s AND chat_id = %s", (date, message.chat.id))
-    notes = cursor.fetchall() # получаем все заметки из базы данных
+    notes = cursor.fetchall()  # получаем все заметки из базы данных
     if notes:
         for note in notes:
-            bot.send_message(message.chat.id, note) # отправляем текст каждой заметки пользователю
+            bot.send_message(message.chat.id, note)  # отправляем текст каждой заметки пользователю
     else:
-        bot.send_message(message.chat.id, 'Заметки на эту дату не найдены') # отправляем сообщение об ошибке, если заметки не найдены
+        bot.send_message(message.chat.id,
+                         'Заметки на эту дату не найдены')  # отправляем сообщение об ошибке, если заметки не найдены
 
 
 @bot.message_handler(commands=['start', 'hello'])
